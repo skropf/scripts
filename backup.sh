97712442adfb57da0026a2b5842101caca5cb44b
simon <simon.kropf@protonmail.com>
2020-07-23 15:51:14 +0200

    added trailing slash + running check

--- backup.sh
+++ backup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+# stop script if it's already running
+# IMPORTANT NOTE: if other software uses rsync this will not be correct!
+nr_procs="$(ps ax | grep rsync | wc -l)"
+if [ "$nr_procs" -ne "1" ]
+then
+    echo "Backup already running. Stopping."
+    exit
+fi
+
+echo "Starting backup."
+
 USER="backup"
 HOST=<destination ip/hostname>
 PRIVATE_KEY="id_ed25519"
 SSH="ssh -i ~/.ssh/$PRIVATE_KEY"
 
 DEST="/mnt/backup"
-SRC="/mnt/HD"
+# trailing slash is important here - otherwise it will create a subfolder
+SRC="/mnt/HD/"
 
 NEW_FOLDER="$(date +%F_%H-%M-%S)"
 
